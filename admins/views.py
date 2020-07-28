8dbde9c709c0c2f35ef08d41aa58e40f0353330b
Stalin Chiguano <stalin_dany98@hotmail.com>
2020-07-28 16:55:02 -0500

    Navegacion entre paginas funcionando al 100

--- admins/views.py
+++ admins/views.py
@@ -1,36 +1,37 @@
 # Django
 from django.shortcuts import render
-from admins.models import Reserva, Anuncios, Promocion
+from admins.models import Reserva as r, Anuncios as a, Promocion as p
 
 
 def QuienesSomos(request):
     return render( request, 'housetime/quienes_somos.html' )
 
-"""
+
 def Anuncios(request):
     return render( request, 'housetime/anuncios.html' )
 
+
 def Contacto(request):
-    return render(request, 'housetime/contacto.html')
+    return render( request, 'housetime/contacto.html' )
 
 
 def Promociones(request):
-    return render(request, 'housetime/promociones.html')
+    return render( request, 'housetime/promociones.html' )
+
 
 def Blog(request):
-    return render(request, 'housetime/blog.html')
+    return render( request, 'housetime/blog.html' )
 
 
 def CompraDetalle(request):
-    return render(request, 'housetime/anuncio_detalle.html')
-"""
+    return render( request, 'housetime/anuncio_detalle.html' )
 
-def housetime(request):
 
-    cantClientes = Reserva.objects.count()
-    cantDepartamentos = Anuncios.objects.filter(id_tipo=2).count()
-    cantCasas = Anuncios.objects.filter(id_tipo=1).count()
-    cantPromociones = Promocion.objects.count()
+def get_housetime(request):
+    cantClientes = r.objects.count()
+    cantDepartamentos = a.objects.filter( id_tipo=2 ).count()
+    cantCasas = a.objects.filter( id_tipo=1 ).count()
+    cantPromociones = p.objects.count()
 
     inventario = [
         {
@@ -41,4 +42,4 @@ def housetime(request):
         }
     ]
 
-    return render(request, 'housetime/index.html', {'inventario': inventario})
+    return render( request, 'housetime/index.html', {'inventario': inventario} )
