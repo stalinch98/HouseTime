c2b087fb3c5b1d35a063d1a01f3d457e2836a3a3
Stalin Chiguano <stalin_dany98@hotmail.com>
2020-07-28 03:16:22 -0500

    Envio de datos de la base al index para animacion de jquery

--- admins/views.py
+++ admins/views.py
@@ -1,7 +1,20 @@
 # Django
 from django.shortcuts import render
-
+from admins.models import Reserva,Anuncios,Promocion
 
 def HouseTime(request):
+    cantClientes = Reserva.objects.count()
+    cantCasas = Anuncios.objects.filter(id_tipo=1).count()
+    cantDepartamentos = Anuncios.objects.filter(id_tipo=2).count()
+    cantPromociones = Promocion.objects.count()
 
-    return render(request, 'housetime/index.html')
+    inventario = [
+        {
+            'clientes':  Reserva.objects.count(),
+            'casas': cantCasas,
+            'departamentos': cantDepartamentos,
+            'promociones': cantPromociones,
+        }
+    ]
+
+    return render(request, 'housetime/index.html', {'inventario': inventario})
